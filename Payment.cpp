441e8fd55b260c00ad2344e19255c5489922de0e
Zubabubal <145017356+Zubabubal@users.noreply.github.com>
2024-12-14 10:28:34 +0100

    Delete Payment.cpp

--- Payment.cpp
+++
@@ -1,8 +0,0 @@
-#include "Payment.h"
-
-Payment::Payment(double amount, const string& method, const string& status)
-    : amount(amount), method(method), status(status) {}
-
-void Payment::print() const {
-    cout << "Payment Amount: $" << amount << "\nMethod: " << method << "\nStatus: " << status << endl;
-}
